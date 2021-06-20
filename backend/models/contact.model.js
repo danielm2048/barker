e90081f78d116802def31ca3bba03cbd1b213b0f
danielm2048 <danielmimoun98@gmail.com>
2021-06-20 21:50:39 +0300

    Added models
    Co-authored-by: ofir-danan <ofir-danan@users.noreply.github.com>

---
+++ backend/models/contact.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const contactsSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		phone: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		type: {
+			type: String,
+			required: true,
+		},
+		location: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("Contacts", contactsSchema);
