7104e7a5100bf28c196ed616e292de77c02138b2
danielm2048 <danielmimoun98@gmail.com>
2021-06-23 23:44:16 +0300

    Basic frontend ract boilerplate with icons

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import barkerLogo from "./images/BarkerLogo.png";
+
+function App() {
+	return <div className="App">
+		<div className="App-header">
+			<img src={barkerLogo} alt="baker logo" style={{width: 500}} />
+		</div>
+	</div>;
+}
+
+export default App;
