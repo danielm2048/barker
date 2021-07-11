02144269c1a25ce4a5342cc99cbd5c6c82c7dd40
danielm2048 <danielmimoun98@gmail.com>
2021-07-11 19:26:18 +0300

    fixed auth menu

--- frontend/src/components/auth/AuthMenu.js
+++ frontend/src/components/auth/AuthMenu.js
@@ -2,76 +2,76 @@ import { useCallback, useEffect, useRef } from "react";
 import Logout from "../auth/Logout";
 
 import {
-  MenuContainer,
-  Menu,
-  MenuItem,
-  MenuLink,
+	MenuContainer,
+	Menu,
+	MenuItem,
+	MenuLink,
 } from "../../styles/StyledMenu";
 import {
-  MenuIcon,
-  UserIcon,
-  HeartIcon,
-  ChatIcon,
+	MenuIcon,
+	UserIcon,
+	HeartIcon,
+	ChatIcon,
 } from "../../styles/StyledIcons";
-import { Plus, MessageCircle } from "@styled-icons/feather";
+import { Plus } from "@styled-icons/feather";
 
 const AuthMenu = ({ authMenuOpen, setAuthMenuOpen, userId }) => {
-  const handleClickOutside = useCallback(
-    (event) => {
-      if (
-        ref.current &&
-        !ref.current.contains(event.target) &&
-        event.target.id !== "menu" &&
-        event.target.parentNode.id !== "menu" &&
-        event.target.parentNode.parentNode.id !== "menu"
-      ) {
-        setAuthMenuOpen(false);
-      }
-    },
-    [setAuthMenuOpen]
-  );
+	const handleClickOutside = useCallback(
+		(event) => {
+			if (
+				ref.current &&
+				!ref.current.contains(event.target) &&
+				event.target.id !== "menu" &&
+				event.target.parentNode.id !== "menu" &&
+				event.target.parentNode.parentNode.id !== "menu"
+			) {
+				setAuthMenuOpen(false);
+			}
+		},
+		[setAuthMenuOpen]
+	);
 
-  const ref = useRef(null);
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [handleClickOutside]);
+	const ref = useRef(null);
+	useEffect(() => {
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [handleClickOutside]);
 
-  return (
-    <>
-      <MenuIcon size="24" title="Menu" />
+	return (
+		<>
+			<MenuIcon size="24" title="Menu" />
 
-      <MenuContainer open={authMenuOpen} ref={ref}>
-        <Menu>
-          <MenuItem>
-            <MenuLink to={`/profile/${userId}`}>
-              <UserIcon size="24" title="Profile" /> My Profile
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink to="/saved">
-              <HeartIcon size="24" title="Saved" /> Saved Dogs
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink to="/messenger" style={{ marginLeft: 6 }}>
-              <ChatIcon size="28" title="Messenger" /> Chat
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink to="/add-dog" style={{ marginLeft: 6 }}>
-              <Plus size="28" title="Add" /> Add a Dog
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <Logout />
-          </MenuItem>
-        </Menu>
-      </MenuContainer>
-    </>
-  );
+			<MenuContainer open={authMenuOpen} ref={ref}>
+				<Menu>
+					<MenuItem>
+						<MenuLink to={`/profile/${userId}`}>
+							<UserIcon size="24" title="Profile" /> My Profile
+						</MenuLink>
+					</MenuItem>
+					<MenuItem>
+						<MenuLink to="/saved">
+							<HeartIcon size="24" title="Saved" /> Saved Dogs
+						</MenuLink>
+					</MenuItem>
+					<MenuItem>
+						<MenuLink to="/messenger" style={{ marginLeft: 6 }}>
+							<ChatIcon size="28" title="Messenger" /> Chat
+						</MenuLink>
+					</MenuItem>
+					<MenuItem>
+						<MenuLink to="/add-dog" style={{ marginLeft: 6 }}>
+							<Plus size="28" title="Add" /> Add a Dog
+						</MenuLink>
+					</MenuItem>
+					<MenuItem>
+						<Logout />
+					</MenuItem>
+				</Menu>
+			</MenuContainer>
+		</>
+	);
 };
 
 export default AuthMenu;
--- frontend/src/styles/StyledMenu.js
+++ frontend/src/styles/StyledMenu.js
@@ -2,46 +2,45 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const MenuContainer = styled.div`
-  display: ${(props) => (props.open ? "block" : "none")};
-  position: fixed;
-  z-index: 1;
-  right: 40px;
-  top: 90px;
-  width: 15rem;
-  /* height: 100%; */
-  border-radius: 15px;
-  background-color: #fefefe;
+	display: ${(props) => (props.open ? "block" : "none")};
+	position: fixed;
+	z-index: 1;
+	right: 40px;
+	top: 90px;
+	width: 15rem;
+	border-radius: 15px;
+	background-color: #fefefe;
 `;
 
 export const Menu = styled.ul`
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
+	list-style-type: none;
+	padding: 0;
+	margin: 0;
 `;
 
 export const MenuItem = styled.li`
-  width: 100%;
-  padding: 15px 0;
-  text-align: left;
+	width: 100%;
+	text-align: left;
 
-  &:hover {
-    background-color: #f5f5f5;
-  }
+	&:hover {
+		background-color: #f5f5f5;
+	}
 
-  &:first-child {
-    border-radius: 15px 15px 0 0;
-  }
+	&:first-child {
+		border-radius: 15px 15px 0 0;
+	}
 
-  &:last-child {
-    border-radius: 0 0 15px 15px;
-  }
+	&:last-child {
+		border-radius: 0 0 15px 15px;
+	}
 `;
 
 export const MenuLink = styled(NavLink)`
-  display: inline-block;
-  width: 100%;
-  color: black;
-  font-weight: 400;
-  text-decoration: none;
-  margin-left: 10px;
+	display: inline-block;
+	width: 100%;
+	color: black;
+	font-weight: 400;
+	text-decoration: none;
+	margin-left: 10px;
+	padding: 15px 0;
 `;
