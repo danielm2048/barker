a18edcce0abd3051897c3f663c10147774be8646
danielm2048 <danielmimoun98@gmail.com>
2021-07-11 19:27:12 +0300

    contact owner button looks better

--- frontend/src/components/layout/SwooferDrawer.js
+++ frontend/src/components/layout/SwooferDrawer.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Drawer, DrawerContent } from "../../styles/StyledDrawer";
+import { Drawer, DrawerContent, LinkButton } from "../../styles/StyledDrawer";
 import Button from "../../styles/StyledButton";
 import { Close } from "../../styles/StyledModal";
 import { HeartIcon } from "../../styles/StyledIcons";
@@ -12,129 +12,140 @@ import useAuth from "../../hooks/useAuth";
 import useAddConversation from "../../hooks/chat/useAddConversation";
 
 const SwooferDrawer = () => {
-  const { open, setOpen, dog, isDogMine } = useDrawerStore();
-  const setIsClicked = useChoiceStore((state) => state.setIsClicked);
+	const { open, setOpen, dog, isDogMine } = useDrawerStore();
+	const setIsClicked = useChoiceStore((state) => state.setIsClicked);
 
-  const { data: user } = useAuth();
-  const { mutate: addConversation } = useAddConversation();
+	const { data: user } = useAuth();
+	const { mutate: addConversation } = useAddConversation();
 
-  if (!dog) {
-    return null;
-  }
+	if (!dog) {
+		return null;
+	}
 
-  return (
-    <Drawer open={open}>
-      <Close onClick={() => setOpen(false)} style={{ right: 35, top: 25 }}>
-        <X size="32" title="Close" />
-      </Close>
+	return (
+		<Drawer open={open}>
+			<Close onClick={() => setOpen(false)} style={{ right: 35, top: 25 }}>
+				<X size="32" title="Close" />
+			</Close>
 
-      <DrawerContent>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            width: "50%",
-            height: "55vh",
-            margin: 10,
-            padding: 10,
-            boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-            borderRadius: 10,
-          }}
-        >
-          <h1 style={{ marginBottom: 5 }}>
-            {dog.name}
-            <Award size="36" color="#f97e7e" />
-          </h1>
-          <h2>{dog.breed}</h2>
-          <h3>
-            {dog.age >= 12
-              ? `${dog.age / 12} Years Old`
-              : `${dog.age} Months Old `}
-          </h3>
-          <p style={{ width: "60%" }}>{dog.info}</p>
-          <span>{dog.gender ? "Female" : "Male"}</span>
-        </div>
+			<DrawerContent>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						width: "65%",
+						height: "55vh",
+						margin: 10,
+						padding: 10,
+						boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+						borderRadius: 10,
+					}}
+				>
+					<h1 style={{ marginBottom: 5 }}>
+						{dog.name}
+						<Award size="36" color="#f97e7e" />
+					</h1>
+					<h2>{dog.breed}</h2>
+					<h3>
+						{dog.age >= 12
+							? `${dog.age / 12} Years Old`
+							: `${dog.age} Months Old `}
+					</h3>
+					<p style={{ width: "60%" }}>{dog.info}</p>
+					<span>{dog.gender ? "Female" : "Male"}</span>
+				</div>
 
-        <div>
-          <Slideshow pics={dog.pics} />
+				<div>
+					<Slideshow pics={dog.pics} />
 
-          {!isDogMine && (
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-evenly",
-                alignItems: "center",
-                margin: 10,
-                boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-                borderRadius: 10,
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  width: "30%",
-                }}
-              >
-                <h3 style={{ marginBottom: 5 }}>
-                  <Link
-                    to={`/profile/${dog.contact?.id}`}
-                    style={{
-                      color: "black",
-                      textDecoration: "none",
-                      fontWeight: "bold",
-                    }}
-                    onClick={() => setOpen(false)}
-                    title="Click here to learn more about the owner"
-                  >
-                    {dog.contact?.name}
-                  </Link>
-                  {dog.contact?.isOrg ? (
-                    <Users size="30" />
-                  ) : (
-                    <User size="30" />
-                  )}
-                </h3>
-                {dog.contact?.phone && (
-                  <h4>
-                    <Phone size="28" />
-                    {dog.contact?.phone}
-                  </h4>
-                )}
-                <h4>
-                  <AtSign size="28" /> {dog.contact?.email}
-                </h4>
-                <p style={{ width: "60%" }}>{dog.contact?.info}</p>
-                <Link
-                  to="/messenger"
-                  onClick={() => {
-                    setOpen(false);
-                    addConversation({
-                      senderId: user.id,
-                      receiverId: dog.contact?.id,
-                    });
-                  }}
-                >
-                  Message owner
-                </Link>
-              </div>
-              <Button
-                circle
-                onClick={() => {
-                  setOpen(false);
-                  setTimeout(() => {
-                    setIsClicked(true, "right");
-                  }, 100);
-                }}
-              >
-                <HeartIcon size="24" />
-              </Button>
-            </div>
-          )}
-        </div>
-      </DrawerContent>
-    </Drawer>
-  );
+					{!isDogMine && (
+						<div
+							style={{
+								display: "flex",
+								justifyContent: "space-around",
+								alignItems: "center",
+								margin: 10,
+								boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+								borderRadius: 10,
+							}}
+						>
+							<div
+								style={{
+									display: "flex",
+									flexDirection: "column",
+									width: "80%",
+								}}
+							>
+								<h3 style={{ marginBottom: 5 }}>
+									<Link
+										to={`/profile/${dog.contact?.id}`}
+										style={{
+											color: "#fd9090",
+											textDecoration: "none",
+											fontWeight: "bold",
+										}}
+										onClick={() => setOpen(false)}
+										title="Click here to learn more about the owner"
+									>
+										{dog.contact?.name}
+									</Link>
+									{dog.contact?.isOrg ? (
+										<Users size="30" color="#fd9090" />
+									) : (
+										<User size="30" color="#fd9090" />
+									)}
+								</h3>
+								{dog.contact?.phone && (
+									<h4>
+										<Phone size="28" />
+										{dog.contact?.phone}
+									</h4>
+								)}
+								<h4>
+									<AtSign size="28" /> {dog.contact?.email}
+								</h4>
+								<p style={{ width: "100%" }}>{dog.contact?.info}</p>
+							</div>
+
+							<div
+								style={{
+									display: "flex",
+									flexDirection: "column",
+									justifyContent: "space-evenly",
+									alignItems: "center",
+								}}
+							>
+								<Button
+									circle
+									onClick={() => {
+										setOpen(false);
+										setTimeout(() => {
+											setIsClicked(true, "right");
+										}, 100);
+									}}
+								>
+									<HeartIcon size="24" />
+								</Button>
+
+								<LinkButton
+									to="/messenger"
+									onClick={() => {
+										setOpen(false);
+										addConversation({
+											senderId: user.id,
+											receiverId: dog.contact?.id,
+										});
+									}}
+								>
+									Message owner
+								</LinkButton>
+							</div>
+						</div>
+					)}
+				</div>
+			</DrawerContent>
+		</Drawer>
+	);
 };
 
 export default SwooferDrawer;
--- frontend/src/styles/StyledDrawer.js
+++ frontend/src/styles/StyledDrawer.js
@@ -1,30 +1,62 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Drawer = styled.div`
-  position: fixed;
-  bottom: 0;
-  height: ${(props) => (props.open ? "90vh" : "0px")};
-  width: 100vw;
+	position: fixed;
+	bottom: 0;
+	height: ${(props) => (props.open ? "90vh" : "0px")};
+	width: 100vw;
 
-  background-color: #f4f4f4;
-  transition: 0.2s;
-  border-radius: 15px 15px 0 0;
-  z-index: 5;
+	background-color: #f4f4f4;
+	transition: 0.2s;
+	border-radius: 15px 15px 0 0;
+	z-index: 5;
 `;
 
 export const DrawerContent = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-  height: 100%;
-  overflow-y: hidden;
-  margin-right: 100px;
-  margin-left: 100px;
+	height: 100%;
+	overflow-y: hidden;
+	margin-right: 100px;
+	margin-left: 100px;
 
-  p {
-    width: 40%;
-    word-wrap: break-word;
-    margin: 20px 0;
-  }
+	h2,
+	h3,
+	h4,
+	p {
+		margin: 5px 0;
+	}
+
+	p {
+		width: 40%;
+		word-wrap: break-word;
+	}
+`;
+
+export const LinkButton = styled(Link)`
+	cursor: pointer;
+	display: inline-block;
+	margin: 5px 0px;
+	position: relative;
+	text-align: center;
+	text-decoration: none;
+	width: 50%;
+	touch-action: manipulation;
+	font-size: 16px;
+	line-height: 20px;
+	font-weight: 600;
+	border-radius: 8px;
+	border-width: 1px;
+	border-style: solid;
+	outline: none;
+	padding: 10px 20px;
+	transition: box-shadow 0.2s ease 0s, -ms-transform 0.1s ease 0s,
+		-webkit-transform 0.1s ease 0s, transform 0.1s ease 0s;
+	-webkit-tap-highlight-color: transparent;
+	border-color: rgb(34, 34, 34);
+	background: rgb(255, 255, 255);
+	color: rgb(34, 34, 34);
 `;
